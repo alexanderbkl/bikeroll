868e41e57b31a83df419f09acd264f3269a83001
unknown <asederado@gmail.com>
2023-02-01 04:34:12 +0100

    ep 17 tutorial

--- app/Http/Controllers/MessagesController.php
+++ app/Http/Controllers/MessagesController.php
@@ -2,18 +2,23 @@
 
 namespace App\Http\Controllers;
 
+use App\Mail\MessageReceived;
+use Illuminate\Support\Facades\Mail;
 
 class MessagesController extends Controller
 {
     public function store()
     {
-        request()->validate([
+        $message = request()->validate([
             'name' => 'required',
             'email' => 'required|email',
             'subject' => 'required',
             'content' => 'required|min:3'
         ]);
 
-        return 'Datos validados';
+        Mail::to('asederado@gmail.com')->queue(new MessageReceived($message));
+
+      //  return new MessageReceived($message);
+        return 'Mensaje enviado';
     }
 }
